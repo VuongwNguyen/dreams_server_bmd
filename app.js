7d1bba72a3590bbe16d22bb0932e1468d06f8466
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-11-12 22:33:59 +0700

    fix: deploy

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // routes
 app.use("/api", require("./routes"));
+app.get("/", async (req, res) => {
+  res.json({
+    status: true,
+    message: "Welcome to the API of Dreams Social Network",
+    data: null,
+  });
+});
 
 // test
 app.get("/", (req, res) => {
